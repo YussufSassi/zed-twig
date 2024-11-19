a4480635dcc2d7521572606e28aa6e765474a7dc
Jannis Borgers <jannis@immerwieder.design>
2024-11-19 13:01:10 +0100

    replace twig-language-server with twiggy-language-server

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use std::{env, fs};
 use zed_extension_api::{self as zed, Result};
 
-const SERVER_PATH: &str = "node_modules/twig-language-server/out/index.js";
-const PACKAGE_NAME: &str = "twig-language-server";
+const SERVER_PATH: &str = "node_modules/twiggy-language-server/dist/server.js";
+const PACKAGE_NAME: &str = "twiggy-language-server";
 
 struct TwigExtension {
     did_find_server: bool,
